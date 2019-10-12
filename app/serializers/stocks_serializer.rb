172a35e80423febf671138564e98e7b83b0da93c
github username <github email address>
2019-10-13 00:27:26 +0100

    removed serializer

--- app/serializers/stocks_serializer.rb
+++ app/serializers/stocks_serializer.rb
@@ -1,18 +1,18 @@
 class StocksSerializer
-  include FastJsonapi::ObjectSerializer
+#   include FastJsonapi::ObjectSerializer
   
-  attributes :id
-  # attribute :name do |object|
-  #   "#{object"}"
-{id: stock["01. symbol"], 
-  open: stock["02. open"], 
-  high: stock["03. high"], 
-  low: stock["04. low"], 
-  price: stock["05. price"], 
-  volume: stock["06. volume"], 
-  latest_trading: stock["0.7 latest trading day"], 
-  previous_close: stock["08. previous close"], 
-  change: stock["09. change"], 
-  change_pc: stock["10. change percent"]}
+#   attributes :id
+#   # attribute :name do |object|
+#   #   "#{object"}"
+# {id: stock["01. symbol"], 
+#   open: stock["02. open"], 
+#   high: stock["03. high"], 
+#   low: stock["04. low"], 
+#   price: stock["05. price"], 
+#   volume: stock["06. volume"], 
+#   latest_trading: stock["0.7 latest trading day"], 
+#   previous_close: stock["08. previous close"], 
+#   change: stock["09. change"], 
+#   change_pc: stock["10. change percent"]}
 
 end
